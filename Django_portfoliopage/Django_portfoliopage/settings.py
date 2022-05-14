38f08f89acbc675aa8295835f32e569904387f83
christianvuye <christian.vuye@gmail.com>
2022-05-14 20:58:20 +0200

    remove hardcoded Django secret key

--- Django_portfoliopage/Django_portfoliopage/settings.py
+++ Django_portfoliopage/Django_portfoliopage/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@dy2g5vda89qcq3qfxuz%kn)u10b_d9q9b@$zyvycw^8whoahy'
+#SECRET_KEY = 'django-insecure-@dy2g5vda89qcq3qfxuz%kn)u10b_d9q9b@$zyvycw^8whoahy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,5 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
 django_heroku.settings(locals())
